a56a908e9b1803b81a50da31ea56e4f1731adc0e
MatiesC-1952678 <maties.claesen@student.uhasselt.be>
2023-05-07 21:56:35 +0200

    remade get_recipes_for query string

--- Frontend/src/services/queries.ts
+++ Frontend/src/services/queries.ts
@@ -109,21 +109,12 @@ export function MACRO_NUTRIENTS_FOR_FOODS(ids: Array<string>): string {
 }
 
 export function GET_RECIPES_FOR(id: string): string {
-	return `SELECT DISTINCT
-		pp_recipes.id as recipeid,
-		raw_recipes.name as recipename,
-		food.id as foodid, 
-		food.naam as foodname,
-		pp_recipes.techniques as techniques
-	FROM food,ingredients_filtered,pp_recipes,raw_recipes
+	return `SELECT *
+	FROM recipes,food_ingredient_linker as fi
 	WHERE (
-		food.id = `+ id +` AND
-		(lower(ingredients_filtered.processed) LIKE CONCAT('%', CONCAT(lower(food.naam), '%'))) AND 
-		pp_recipes.ingredient_ids ~ CONCAT('(?<=\\D)',CONCAT(ingredients_filtered.id, '(?=\\D)')) AND
-		pp_recipes.id = raw_recipes.id 
-	)
-	ORDER BY recipename
-	--LIMIT 100`;
+		fi.food_id = `+id+` AND
+		fi.ingredient_id = ANY (recipes.ingredient_ids)
+	)`;
 }
 
 export function GET_INGREDIENTS_FOR(id: string): string {
